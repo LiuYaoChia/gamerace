49f2200e03b173d945f96b468256bdd47c14c8ac
LiuYaoChia <b10527042@gmail.com>
2025-08-28 15:57:58 +0800

    Update happiness-game.js

--- Game-setting/happiness-game.js
+++ Game-setting/happiness-game.js
@@ -407,7 +407,7 @@ async function startGame() {
 
 if (isPhone) {
   els.startBtn.style.display = "none";
-  els.resetBtn.style.display = "none"
+  els.resetBtn.style.display = "none";
 } else {
   els.startBtn?.addEventListener("click", async () => {
     const pw = prompt("請輸入管理密碼才能開始遊戲:");
@@ -488,6 +488,7 @@ els.renameBtn?.addEventListener("click", async () => {
 
 // ====== Boot ======
 showSetup();
+
 
 
 
