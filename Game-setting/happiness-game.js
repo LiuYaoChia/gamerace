d73636c3caa1dbd5e11d19466615c77dc5771582
LiuYaoChia <b10527042@gmail.com>
2025-12-05 16:43:14 +0800

    Update happiness-game.js

--- Game-setting/happiness-game.js
+++ Game-setting/happiness-game.js
@@ -945,7 +945,7 @@ function renderGameScene(groups) {
     `;
     label.style.cssText = `
       position: absolute;
-      left: 60px;
+      left: 100px;
       top: laneRect.top + 10 + "px";
       font-weight: 700;
       padding: 6px 12px;
@@ -1512,6 +1512,7 @@ async function removeRedundantGroups() {
   await removeExtraGroups();       // remove any leftover 6th group
   if (!isHost) await renderGroupChoices();
 })();
+
 
 
 
